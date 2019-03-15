dede7df6b80817fb050103890bb930e23bfa54b9
张贝 <mac@192.168.0.117>
2019-03-15 22:31:45 +0800

    去除多余代码

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import AppLayout from './layout/app'
-import Count from './component/count'
-import List from './component/list'
-import Login from './component/Login'
-import Nav from './component/nav'
-import NoticeBoard from './component/noticeBoard'
-import ContextComponent from './component/context/index'
-import Refs from './component/refs'
-import Fragment from './component/fragments'
-import Dialog from './component/dialogExample'
-import AntDialog from './component/antDialog'
 import './page-less/index.css'
 
 class App extends Component {
@@ -18,42 +8,10 @@ class App extends Component {
         super(props)
     }
 
-    dialogClick = () => {
-        console.log('dialog click')
-    }
-
     render () {
-        let list = [
-            {
-                title: '1'
-            },
-            {
-                title: '2'
-            },
-            {
-                title: '3'
-            },
-            {
-                title: '4'
-            },
-            {
-                title: '5'
-            }
-        ]
         return (
             <div>
-                {/* <ContextComponent/>
-                <Refs/>
-                <ul>
-                    <Fragment list={list}/>
-                </ul>
-                <div onClick={this.dialogClick}>
-                    <Dialog/>
-                </div>
-                <AntDialog/> */}
-                <h1 className="page-title">招生公告</h1>
-                <NoticeBoard/>
-                {/* <AppLayout/> */}
+                <AppLayout/>
             </div>
         )
     }
