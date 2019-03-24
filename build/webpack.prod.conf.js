dea14f940777801e0fda5ca40b7e04490d7d67de
张贝 <mac@zhangbei.local>
2019-03-24 23:55:15 +0800

    webpack 配置

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const baseConfig = require('./webpack.base.conf.js')
 
 module.exports = merge(baseConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ]
 })
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppLayout from './layout/app'
 import Basic from './component/router/basic'
 import Nested from '@/component/router/nested'
 import ImgDisplay from '@/component/img'
+import SvgIcon from '@/component/svgIcon'
 import './page-less/index.css'
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         return (
             <div>
                 <ImgDisplay width='200px' height='200px' src='https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=7e685ef2f903918fc8d13bca613d264b/b3119313b07eca80787730f59f2397dda14483b5.jpg'/>
+                <SvgIcon iconClass='icon-paihangbang'/>
             </div>
         )
     }
