33c73985d36408ad9c686b2205abd8bf8aba6e5a
张贝 <mac@zhangbei.local>
2019-04-13 22:19:34 +0800

    eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:promise/recommended"
-  ],
-  plugins: ["standard", "react", "promise", "json"],
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  rules: {
-    indent: ["error", 4]
-  },
-  globals: {
-    // configure global variables avoid no-undef error
-    API: true,
-    GLOBAL: true
-  }
+    parser: "babel-eslint",
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:promise/recommended"
+    ],
+    plugins: ["standard", "react", "promise", "json"],
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    rules: {
+        indent: ["error", 4],
+        "space-before-function-paren": 1,
+        "key-spacing": [
+            2,
+            { beforeColon: false, mode: "script", align: "colon" }
+        ]
+    },
+    globals: {
+        // configure global variables avoid no-undef error
+        API: true,
+        GLOBAL: true
+    }
 };
