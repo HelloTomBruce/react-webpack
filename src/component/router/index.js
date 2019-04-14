f58ee83fcc6944f58c82219663c1539263cc6362
张贝 <mac@zhangbei.local>
2019-04-14 17:05:39 +0800

    react router

--- src/component/router/index.js
+++ src/component/router/index.js
@@ -1,11 +1,12 @@
-import React from "react"
-import UrlParams from "./urlParams"
-import Redirect from "./redirect"
-import CustomLink from "./customLink"
-import Prompt from "./prompt"
-import Recursive from "./Recursive"
-import SideBar from "./sidebar"
-import Nesting from "./nesting"
-import ModalSwitch from "./modalSwitch"
+// import React from "react";
+// import UrlParams from "./urlParams";
+// import Redirect from "./redirect";
+// import CustomLink from "./customLink";
+// import Prompt from "./prompt";
+// import Recursive from "./Recursive";
+// import SideBar from "./sidebar";
+// import Nesting from "./nesting";
+// import ModalSwitch from "./modalSwitch";
+import StaticRouter from "./static";
 
-export default ModalSwitch
+export default StaticRouter;
