26c66625b6038e53b042d1ecec92c255e69e68c3
yingwuzhang <yingwu.zhang@coolcollege.cn>
2019-05-07 15:51:46 +0800

    user by id

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,13 @@
 import client from "../utils/graphql";
-import * as User from "./user.graphql";
+import * as userList from "./userList.graphql";
+import * as userById from "./userById.graphql";
 
 export const getUserList = params =>
-    client.query({ query: User, variables: params });
+    client.query({ query: userList, variables: params });
+
+export const getUserById = params => {
+    return client.query({
+        query:     userById,
+        variables: params
+    });
+};
--- src/page/user/index.js
+++ src/page/user/index.js
@@ -1,28 +1,80 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getUserById } from "@/graphql";
+import { showErrorTip } from "@/redux/action/error";
 import img from "@/img/touxiang.jpg";
+import "./index.less";
+
+const mapStateToProps = () => {
+    return {};
+};
+
+const mapDispatchToMethods = dispatch => {
+    return {
+        showErrorTip: msg => dispatch(showErrorTip(msg))
+    };
+};
 
 class User extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userId: ""
+            user: {
+                id:     "",
+                name:   "",
+                sex:    "",
+                intro:  "",
+                skills: []
+            }
         };
     }
     componentDidMount() {
-        let userId = this.props.match.params;
-        this.setState({
-            userId
-        });
+        let userId = this.props.match.params.id;
+        getUserById({
+            id: +userId
+        })
+            .then(res => {
+                this.setState({
+                    user: res.data.user
+                });
+                return;
+            })
+            .catch(err => {
+                this.props.showErrorTip(err);
+            });
     }
     render() {
         return (
             <div className="userPage">
-                <div className="userPage-top">
-                    <img className="userPage-top-img" src={img} />
+                <div className="userInfo">
+                    <div className="userInfo-top">
+                        <img className="userInfo-top-img" src={img} />
+                    </div>
+                    <div className="userInfo-bottom">
+                        <div className="userInfo-bottom-item">
+                            <label>姓名：</label>
+                            <span>{this.state.user.name}</span>
+                        </div>
+                        <div className="userInfo-bottom-item">
+                            <label>性别：</label>
+                            <span>{this.state.user.sex}</span>
+                        </div>
+                        <div className="userInfo-bottom-item">
+                            <label>介绍：</label>
+                            <p>{this.state.user.intro}</p>
+                        </div>
+                        <div className="userInfo-bottom-item">
+                            <label>爱好与特长：</label>
+                            <p>{this.state.user.skills.join(",")}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
     }
 }
 
-export default User;
+export default connect(
+    mapStateToProps,
+    mapDispatchToMethods
+)(User);
